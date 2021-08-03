09f6b4899c19b853aaf9c5686bc8351224ed45a8
minhchau <minhchau@minhchaus-MacBook-Pro.local>
2021-08-03 21:39:31 +0700

    layout chart finish

---
+++ src/components/dummyData.jsx
@@ -0,0 +1,47 @@
+export const userData = [{
+        name:"Jan",
+        "Active users": 4000
+    },
+    {
+        name:"Feb",
+        "Active users": 3500
+    },{
+        name:"Mar",
+        "Active users": 2568
+    },{
+        name:"Apr",
+        "Active users": 5600
+    },
+    {
+        name:"May",
+        "Active users": 3000
+    },
+    {
+        name:"Jun",
+        "Active users": 2590
+    },
+    {
+        name:"Jul",
+        "Active users": 4444
+    },
+    {
+        name:"Aug",
+        "Active users": 6000
+    },
+    {
+        name:"Sep",
+        "Active users": 4500
+    },
+    {
+        name:"Oct",
+        "Active users": 3450
+    },
+    {
+        name:"Nov",
+        "Active users": 5000
+    },
+    {
+        name:"Dec",
+        "Active users": 4000
+    },
+]
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import './home.css';
 import Features from '../../components/features/Features';
-
+import Chart from '../../components/chart/Chart';
+import {userData} from "../../components/dummyData";
 const home = () => {
     return (
         <div className="homepage">
             <Features />
+            <Chart data={userData} grid title="User Analytics" dataKey="Active users" />
         </div>
     );
 };
